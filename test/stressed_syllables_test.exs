87b4e1732852035451549b0ee0ab24e328c6d43e
Rudi Chen <rudichen@gmail.com>
2017-06-23 13:07:00 -0400

    Get some basic tests working

--- test/stressed_syllables_test.exs
+++ test/stressed_syllables_test.exs
@@ -2,13 +2,72 @@ defmodule StressedSyllablesTest do
   use ExUnit.Case
   doctest StressedSyllables
 
-  import StressedSyllables.CLI, only: [ parse_args: 1 ]
+  alias StressedSyllables.MerriamWord.Word
+  import StressedSyllables.MerriamWord, only: [ parse: 4 ]
 
-  test "displays help if now arguments" do
-    assert parse_args([]) == :help
+  test "parse a normal word" do
+    result = parse(
+      "constitution",
+      "noun",
+      "con·sti·tu·tion",
+      "\\ˌkän(t)-stə-ˈtü-shən, -ˈtyü-\\")
+    %Word{ pofspeech: pofspeech, syllables: syllables, pronounciation: pronounciation } = result
+    assert pofspeech == "NOUN"
+    assert syllables == ["con", "sti", "tu", "tion"]
+    assert pronounciation == ["ˌkän(t)", "stə", "ˈtü", "shən"]
   end
 
-  test "parses the input file" do
-    assert parse_args([ "text.txt" ]) == "text.txt"
+  test "parse geographical name" do
+    result = parse(
+      "colorado",
+      "name",
+      "Col·o·ra·do",
+      "\\ˌkä-lə-ˈra-(ˌ)dō, chiefly by outsiders -ˈrä-\\")
+    %Word{ pofspeech: pofspeech } = result
+    assert pofspeech == "PNOUN"
+  end
+
+  test "parse plural noun" do
+    result = parse(
+      "politics",
+      "noun, plural in form but singular or plural in construction",
+      "pol·i·tics",
+      "\\ˈpä-lə-ˌtiks\\")
+    %Word{ pofspeech: pofspeech } = result
+    assert pofspeech == "NOUN"
+  end
+
+  test "parse attributive noun" do
+    result = parse(
+      "company",
+      "noun, often attributive",
+      "com·pa·ny",
+      "\\ˈkəmp-nē, ˈkəm-pə-\\")
+    %Word{ pofspeech: pofspeech } = result
+    assert pofspeech == "NOUN"
+  end
+
+  test "parse transitive verb" do
+  end
+
+  test "parse word with pronounciation data" do
+  end
+
+  test "parse word with incomplete pronounciation data" do
+    result = parse(
+      "meaningful",
+      "adjective",
+      "mean·ing·ful",
+      "\\-fəl\\")
+    assert result == nil
+  end
+
+  test "parse word with no syllables" do
+    result = parse(
+      "or",
+      "conjunction",
+      "",
+      "\\ər, ˈȯr, Southern also ˈär\\")
+    assert result == nil
   end
 end
