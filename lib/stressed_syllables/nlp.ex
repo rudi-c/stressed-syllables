dea47d44b322826196a3ed0dbb66dad8eda60ba8
Rudi Chen <rudichen@gmail.com>
2017-06-23 13:06:23 -0400

    Measure time taken for Python actions

--- lib/stressed_syllables/nlp.ex
+++ lib/stressed_syllables/nlp.ex
@@ -19,13 +19,18 @@ defmodule StressedSyllables.NLP do
 
   def handle_cast(:load_model, python_process) do
     Logger.info "Loading Spacy NLP model..."
-    :python.call(python_process, @script, :load_model, [])
-    Logger.info "Loaded Spacy NLP model..."
+    {time, _} = :timer.tc(fn ->
+      :python.call(python_process, @script, :load_model, []) end)
+    Logger.info "Loaded Spacy NLP model in #{time / 1000}ms"
     {:noreply, python_process}
   end
 
   def handle_call({:analyze_text, text}, _from, python_process) do
-    result = :python.call(python_process, @script, :analyze_text, [text])
+    {time, result} = :timer.tc(fn ->
+      :python.call(python_process, @script, :analyze_text, [text]) end)
+
+    Logger.info "Did NLP analysis on #{String.length(text)} chars in #{time / 1000}ms"
+
     # Erlang returns char lists, want (unicode) strings instead
     elixir_result =
       result
