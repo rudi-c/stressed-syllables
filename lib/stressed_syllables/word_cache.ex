e4fa93f499cdb8558b5e6f7eb2ff8532acab041f
Rudi Chen <rudichen@gmail.com>
2017-07-07 12:12:39 -0400

    Directly call cache GenServer instead of using Task

--- lib/stressed_syllables/word_cache.ex
+++ lib/stressed_syllables/word_cache.ex
@@ -14,19 +14,27 @@ defmodule StressedSyllables.WordCache do
   end
 
   def store_word(word, result) do
-    Logger.info "Storing word \"#{inspect word}\""
     GenServer.cast(__MODULE__, {:store_word, type_token(), word, result})
   end
 
-  def update_cache(word, result) do
-    Logger.info "Updating cache \"#{inspect word}\""
-    GenServer.cast(__MODULE__, {:update_cache, word, result})
+  defp send_all() do
+    GenServer.cast(__MODULE__, :send_all)
   end
 
-  def send_all() do
-    Logger.info "Sending everything"
-    GenServer.cast(__MODULE__, :send_all)
-    Logger.info "Done sending everything"
+  defp cast_on_master(message) do
+    case Node.list() do
+      [] -> :ok
+      [master] ->
+        GenServer.cast({__MODULE__, master}, message)
+    end
+  end
+
+  defp cast_on_replica(message) do
+    case Node.list() do
+      [] -> :ok
+      [replica] ->
+        GenServer.cast({__MODULE__, replica}, message)
+    end
   end
 
   def start_link() do
@@ -52,7 +60,7 @@ defmodule StressedSyllables.WordCache do
 
       ret = GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
 
-      call_on_master(:send_all, [])
+      cast_on_master(:send_all)
 
       ret
     end
@@ -67,45 +75,32 @@ defmodule StressedSyllables.WordCache do
   end
 
   def handle_cast({:store_word, :master, word, result}, {cache, file}) do
+    Logger.info "Want to store word on master \"#{inspect word}\""
     persist_word(file, word, result)
-    # Send update to replica
-    call_on_replica(:update_cache, [word, result])
+    cast_on_replica({:update_cache, word, result})
     {:noreply, {Map.put(cache, word, result), file}}
   end
 
   def handle_cast({:store_word, :replica, word, result}, cache) do
-    # Send update to master
-    call_on_master(:store_word, [word, result])
+    Logger.info "Want to store word on replica \"#{inspect word}\""
+    cast_on_master({:store_word, :master, word, result})
     {:noreply, cache}
   end
 
   def handle_cast({:update_cache, word, result}, cache) do
+    Logger.info "Updating cache \"#{inspect word}\""
     {:noreply, Map.put(cache, word, result)}
   end
 
   def handle_cast(:send_all, {cache, file}) do
-    Enum.each(cache, fn {word, result} ->
-      call_on_replica(:store_word, [word, result])
-    end)
+    if length(Node.list()) > 0 do
+      Logger.info "Sending everything"
+      Enum.each(cache, fn {word, result} ->
+        cast_on_replica({:update_cache, word, result})
+      end)
+      Logger.info "Done sending everything"
+    end
     {:noreply, {cache, file}}
-  end
-
-  defp call_on_master(function, args) do
-    case Node.list() do
-      [] -> :ok
-      [master] ->
-        Task.Supervisor.async({StressedSyllables.RemoteTasks, master},
-                               StressedSyllables.WordCache, function, args)
-    end
-  end
-
-  defp call_on_replica(function, args) do
-    case Node.list() do
-      [] -> :ok
-      [replica] ->
-        Task.Supervisor.async({StressedSyllables.RemoteTasks, replica},
-                               StressedSyllables.WordCache, function, args)
-    end
   end
 
   defp type_token() do
